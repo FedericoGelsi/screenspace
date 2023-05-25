de174ddb7c160a62a592da62d6d841c541238525
Ivan Chan <ivanchan.194@gmail.com>
2023-05-25 00:36:02 -0300

    Conexion de prueba con DynamoDB

--- src/main/java/com/uade/ad/AdApplication.java
+++ src/main/java/com/uade/ad/AdApplication.java
@@ -1,9 +1,19 @@
 package com.uade.ad;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Screenspace API"),
+		servers = {
+				@Server(url = "http://localhost:5000"),
+				@Server(url = "http://screenspace-api.us-east-1.elasticbeanstalk.com")
+		}
+)
 public class AdApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/uade/ad/controller/dto/LoginDto.java
@@ -0,0 +1,14 @@
+package com.uade.ad.controller.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+
+    private String username;
+
+    private String password;
+}
