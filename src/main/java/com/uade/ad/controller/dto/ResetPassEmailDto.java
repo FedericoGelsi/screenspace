8cc1a11c801fe275e4ab4efd9e8bf6011e3b13f4
mati <matias.cespedes@outlook.com>
2023-06-24 21:47:21 -0300

    modificacion respuesta login

---
+++ src/main/java/com/uade/ad/controller/dto/ResetPassEmailDto.java
@@ -0,0 +1,12 @@
+package com.uade.ad.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPassEmailDto {
+    private String email;
+}
